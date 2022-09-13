d122642da1e85119e43435d53833fc9139bff922
OmphileMonchwe <omphile05monchwe@gmail.com>
2022-09-13 14:59:42 +0200

    dp c

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  http_timeout: 500, //unit: seconds, 0: no timeout
+  apiUrl: "https://simamisa.herokuapp.com/simamisa/orphanages"
 };
