21b6c691a4cc1408d0b8ed61b526bb8e76cb091b
OmphileMonchwe <omphile05monchwe@gmail.com>
2022-09-14 17:05:36 +0200

    send offer fixed

--- src/app/others/modal-make-offer/modal-make-offer.component.ts
+++ src/app/others/modal-make-offer/modal-make-offer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DataToModalsService } from 'src/app/services/data-to-modals.service';
 import { OffersService } from 'src/app/services/offers.service';
 
 @Component({
@@ -9,33 +10,42 @@ import { OffersService } from 'src/app/services/offers.service';
 })
 export class ModalMakeOfferComponent implements OnInit {
 
-  constructor(private offersService: OffersService,private router: Router) { }
+  constructor(private offersService: OffersService,private router: Router, private dataToModals: DataToModalsService) { }
 
   ngOnInit(): void {
   }
   makeOffer( offerDetails: { title: String;  quantity: number; description: string}) {
 
-
+   console.log("About to make an offer");
+    this.dataToModals.makeOfferDatasent$.subscribe(data=>{ // data is the id to sent an offer to
+     
+    }
+    );
+  var fromOrphanageID = Number(localStorage.getItem("orphID"));
+  console.log("RecID",localStorage.getItem("recID") )
 
     let Offer = {
-      from:  1,
-      to: 5,
+      from:  fromOrphanageID,
+      to:   localStorage.getItem("recID"),
       Title: offerDetails.title,
       Description: offerDetails.description ,
       Quantity: offerDetails.quantity,
 
     };
 
-    console.log(Offer);
+    console.log("MyOffer",Offer);
     const body = JSON.stringify(Offer);
-
+    window.alert(localStorage.getItem("recID"));
     this.offersService.MakeOffer(body).subscribe(data => {
       console.log(data);
-        
-      this.router.navigate(['./home']);
-
+      window.alert("The offer was succefully sent.");
+      localStorage.removeItem("recID");
+      window.location.reload();
+      //this.router.navigate(['./home']);
+      
 
   });
+
 
  }
  
--- src/app/others/view-mypartners/view-mypartners.component.ts
+++ src/app/others/view-mypartners/view-mypartners.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Orphanage } from 'src/app/models/orphanage';
+import { DataToModalsService } from 'src/app/services/data-to-modals.service';
 import { OrphanageService } from 'src/app/services/orphanage.service';
 import { PartneringService } from 'src/app/services/partnering.service';
 
@@ -16,7 +17,7 @@ export class ViewMypartnersComponent implements OnInit {
   orphanages?: Orphanage[];
   formModal:any;
   makeOfferModal: any;
-  constructor(private partneringService: PartneringService) { }
+  constructor(private partneringService: PartneringService, private dataToModels: DataToModalsService) { }
 
   ngOnInit(): void {
     console.log("Initializing my partners");
@@ -32,6 +33,10 @@ export class ViewMypartnersComponent implements OnInit {
  }
  openMakeOfferModal(){
    this.makeOfferModal.show();
+ }
+ selected(orphanage: Orphanage){
+   // this.dataToModels.setToOrphanageID(orphanage.ID);
+    localStorage.setItem("recID",orphanage.ID.toString());
  }
 
 }
