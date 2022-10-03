30e0c6add766602303775f68e094e4e5998a9fbc
OmphileMonchwe <omphile05monchwe@gmail.com>
2022-10-03 09:52:29 +0200

    partnering loaders

--- src/app/pages/manager/my-offers/my-offers.component.ts
+++ src/app/pages/manager/my-offers/my-offers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Offer } from 'src/app/models/offer';
+import { LoaderService } from 'src/app/services/loader.service';
 import { OffersService } from 'src/app/services/offers.service';
 
 @Component({
@@ -10,16 +11,23 @@ import { OffersService } from 'src/app/services/offers.service';
 })
 export class MyOffersComponent implements OnInit {
   offers?: Offer[]
-  constructor(private offersService: OffersService) { }
+  noData:boolean=false;
+  isLoading = false;
+  constructor(private offersService: OffersService,public loaderService:LoaderService) { }
 
   ngOnInit(): void {
+    this.loaderService.isLoading.subscribe((data)=>{
+      this.isLoading = data;
+    })
     this.offersService.getMyOffer().subscribe(data=>{
-      
+     
       this.offers =data;
+      if(data.length == 0){
+        this.noData = this.nothingReturned();}
     });
   }
   nothingReturned(): boolean{
-    if(this.offers?.length == 0){
+    if(this.offers?.length == 0 && !this.isLoading){
      return true;
     }
     else{
--- src/app/pages/manager/my-partners/my-partners.component.ts
+++ src/app/pages/manager/my-partners/my-partners.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { da } from 'date-fns/locale';
 import { Orphanage } from 'src/app/models/orphanage';
+import { LoaderService } from 'src/app/services/loader.service';
 import { PartneringService } from 'src/app/services/partnering.service';
 
 @Component({
@@ -11,17 +12,24 @@ import { PartneringService } from 'src/app/services/partnering.service';
 })
 export class MyPartnersComponent implements OnInit {
   orphanages!: Orphanage[];
-  constructor(private partneringService: PartneringService) { }
+  noData=false;
+  isLoading= false;
+  constructor(private partneringService: PartneringService,public loaderService:LoaderService) { }
 
   ngOnInit(): void {
+    this.loaderService.isLoading.subscribe((data)=>{
+      this.isLoading=data;
+    })
     this.partneringService.GetMyPartners().subscribe(data=>{
       this.orphanages =data;
+      if(data.length == 0){
+        this.noData = this.nothingReturned();}
       console.log("Data received:", this.orphanages);
     });
   
   }
   nothingReturned(): boolean{
-     if(this.orphanages.length == 0){
+     if(!this.isLoading && this.orphanages.length == 0){
       return true;
      }
      else{
